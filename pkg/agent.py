6e0d74878f93435f03b9cdb51d4cf54cf89d1023
Jean <jeanrodrigues0908@gmail.com>
2020-05-29 18:39:40 -0300

    commit inicial

---
+++ pkg/agent.py
@@ -0,0 +1,283 @@
+from model import Model
+from problem import Problem
+from state import State
+from cardinal import action
+from tree import TreeNode
+
+UNIFORME_COST = 0
+A_START_1 = 1
+A_START_2 = 2
+
+# Funções utilitárias
+def printExplored(explored):
+    """Imprime os nós explorados pela busca.
+    @param explored: lista com os nós explorados."""
+    #@TODO: Implementação do aluno
+    print("--- Explorados --- (TAM: {})".format(len(explored)))
+    for state in explored:
+        print(state,end=' ')
+    print("\n")
+
+def printFrontier(frontier):
+    """Imprime a fronteira gerada pela busca.
+    @param frontier: lista com os nós da fronteira."""
+    #@TODO: Implementação do aluno
+    print("--- Fronteira --- (TAM: {})".format(len(frontier)))
+    for node in frontier:
+        print(node,end=' ')
+    print("\n")
+
+def buildPlan(solutionNode):
+    #@TODO: Implementação do aluno
+    depth = solutionNode.depth
+    solution = [0 for i in range(depth)]
+    parent = solutionNode
+
+    for i in range(len(solution) - 1, -1, -1):
+        solution[i] = parent.action
+        parent = parent.parent
+    return solution
+
+
+class Agent:
+    """"""
+    counter = -1 # Contador de passos no plano, usado na deliberação
+
+    def __init__(self, model):
+        """Construtor do agente.
+        @param model: Referência do ambiente onde o agente atuará."""
+        self.model = model
+
+        self.prob = Problem()
+        self.prob.createMaze(9, 9)
+        self.prob.mazeBelief.putVerticalWall(0,1,0)
+        self.prob.mazeBelief.putVerticalWall(0,0,1)
+        self.prob.mazeBelief.putVerticalWall(5,8,1)
+        self.prob.mazeBelief.putVerticalWall(5,5,2)
+        self.prob.mazeBelief.putVerticalWall(8,8,2)
+
+        self.prob.mazeBelief.putHorizontalWall(4,7,0)
+        self.prob.mazeBelief.putHorizontalWall(7,7,1)
+        self.prob.mazeBelief.putHorizontalWall(3,5,2)
+        self.prob.mazeBelief.putHorizontalWall(3,5,3)
+        self.prob.mazeBelief.putHorizontalWall(7,7,3)
+
+        self.prob.mazeBelief.putVerticalWall(6,7,4)
+        self.prob.mazeBelief.putVerticalWall(5,6,5)
+        self.prob.mazeBelief.putVerticalWall(5,7,7)
+
+        # Posiciona fisicamente o agente no estado inicial
+        initial = self.positionSensor()
+        self.prob.defInitialState(initial.row, initial.col)
+        
+        # Define o estado atual do agente = estado inicial
+        self.currentState = self.prob.initialState
+
+        # Define o estado objetivo
+        self.prob.defGoalState(2, 8)
+        self.model.setGoalPos(2,8)
+
+        # Plano de busca
+        self.plan = None
+
+    def printPlan(self):
+        """Apresenta o plano de busca."""    
+        print("--- PLANO ---")
+        # @TODO: Implementação do aluno
+        for plannedAction in self.plan:
+            print("{} > ".format(action[plannedAction]),end='')
+        print("FIM\n\n")
+
+    def deliberate(self):
+        # Primeira chamada, realiza busca para elaborar um plano
+        # @TODO: Implementação do aluno
+        if self.counter == -1: 
+            self.plan = self.cheapestFirstSearch(0) # 0 = custo uniforme, 1 = A* com colunas, 2 = A* com dist Euclidiana
+            if self.plan != None:
+                self.printPlan()
+            else:
+                print("SOLUÇÃO NÃO ENCONTRADA")
+                return -1
+
+        # Nas demais chamadas, executa o plano já calculado
+        self.counter += 1
+
+        # Atingiu o estado objetivo 
+        if self.prob.goalTest(self.currentState):
+            print("!!! ATINGIU O ESTADO OBJETIVO !!!")
+            return -1
+        # Algo deu errado, chegou ao final do plano sem atingir o objetivo
+        if self.counter >= len(self.plan):
+            print("### ERRO: plano chegou ao fim, mas objetivo não foi atingido.")
+            return -1
+        currentAction = self.plan[self.counter]
+
+        print("--- Mente do Agente ---")
+        print("{0:<20}{1}".format("Estado atual :",self.currentState))
+        print("{0:<20}{1} de {2}. Ação= {3}\n".format("Passo do plano :",self.counter + 1,len(self.plan),action[currentAction]))
+        self.executeGo(currentAction)
+
+        # Atualiza o estado atual baseando-se apenas nas suas crenças e na função sucessora
+        # Não faz leitura do sensor de posição
+        self.currentState = self.prob.suc(self.currentState, currentAction)
+        return 1
+
+    def executeGo(self, direction):
+        """Atuador: solicita ao agente física para executar a ação.
+        @param direction: Direção da ação do agente
+        @return 1 caso movimentação tenha sido executada corretamente."""
+        self.model.go(direction)
+        return 1
+
+    def positionSensor(self):
+        """Simula um sensor que realiza a leitura do posição atual no ambiente e traduz para uma instância da classe Estado.
+        @return estado que representa a posição atual do agente no labirinto."""
+        pos = self.model.agentPos
+        return State(pos[0],pos[1])
+
+    def hn1(self, state):
+        """Implementa uma heurísitca - número 1 - para a estratégia A*.
+        No caso hn1 é a distância em coluna do estado passado com argumento até o estado objetivo.
+        @param state: estado para o qual se quer calcular o valor de h(n)."""
+        # @TODO: Implementação do aluno
+        return abs( state.col - self.prob.goalState.col )
+    
+    def hn2(self, state):
+        """Implementa uma heurísitca - número 2 - para a estratégia A*.
+        No caso hn1 é a distância distância euclidiana do estado passado com argumento até o estado objetivo.
+        @param state: estado para o qual se quer calcular o valor de h(n)."""
+        # @TODO: Implementação do aluno
+        distCol = abs(state.col - self.prob.goalState.col)
+        distRow = abs(state.row - self.prob.goalState.row)
+        squared = distRow**2 + distCol**2
+        return squared ** 0.5
+
+    def cheapestFirstSearch(self, searchType):
+        """Realiza busca com a estratégia de custo uniforme ou A* conforme escolha realizada na chamada.
+        @param searchType: 0=custo uniforme, 1=A* com heurística hn1; 2=A* com hn2
+        @return plano encontrado"""
+        # @TODO: Implementação do aluno
+        # Atributos para análise de desempenho
+        treeNodesCt = 0 # contador de nós gerados incluídos na árvore
+        # nós inseridos na árvore, mas que não necessitariam porque o estado 
+        # já foi explorado ou por já estar na fronteira
+        exploredDicardedNodesCt = 0
+        frontierDiscardedNodesCt = 0 
+
+        # Algoritmo de busca
+        solution = None
+        root = TreeNode(parent=None)
+        root.state = self.prob.initialState
+        root.gn = 0
+        root.hn = 0
+        root.action = -1 
+        treeNodesCt += 1
+
+        # cria FRONTEIRA com estado inicial
+        frontier = []
+        frontier.append(root)
+
+        # cria EXPLORADOS - inicialmente vazia
+        explored = []
+
+        print("\n*****\n***** INICIALIZAÇÃO ÁRVORE DE BUSCA\n*****\n")
+        print("\n{0:<30}{1}".format("Nós na árvore: ",treeNodesCt))
+        print("{0:<30}{1}".format("Descartados já na fronteira: ",frontierDiscardedNodesCt))
+        print("{0:<30}{1}".format("Descartados já explorados: ",exploredDicardedNodesCt))
+        print("{0:<30}{1}".format("Total de nós gerados: ",treeNodesCt + frontierDiscardedNodesCt + exploredDicardedNodesCt))
+
+        while len(frontier): # Fronteira não vazia
+            print("\n*****\n***** Início iteração\n*****\n")
+            printFrontier(frontier)
+
+            selNode = frontier.pop(0) # retira nó da fronteira
+            selState = selNode.state
+            print("Selecionado para expansão: {}\n".format(selNode))
+
+            # Teste de objetivo
+            if selState == self.prob.goalState:
+                solution = selNode
+                break
+            explored.append(selState)
+            printExplored(explored)
+
+            # Obtem ações possíveis para o estado selecionado para expansão
+            actions = self.prob.possibleActions(selState) # actions é do tipo [-1, -1, -1, 1, 1, -1, -1, -1]
+            
+            for actionIndex, act in enumerate(actions):
+                if(act < 0): # Ação não é possível
+                    continue
+
+                # INSERE NÓ FILHO NA ÁRVORE DE BUSCA - SEMPRE INSERE, DEPOIS
+                # VERIFICA SE O INCLUI NA FRONTEIRA OU NÃO
+                # Instancia o filho ligando-o ao nó selecionado (nSel)  
+                child = selNode.addChild()
+                # Obtem o estado sucessor pela execução da ação <act>
+                sucState = self.prob.suc(selState, actionIndex)
+                child.state = sucState
+                # Custo g(n): custo acumulado da raiz até o nó filho
+                gnChild = selNode.gn + self.prob.getActionCost(actionIndex)
+                if searchType == UNIFORME_COST:
+                    child.setGnHn(gnChild, 0) # Deixa h(n) zerada porque é busca de custo uniforme
+                elif searchType == A_START_1:
+                    child.setGnHn(gnChild, self.hn1(sucState) )
+                elif searchType == A_START_2:
+                    child.setGnHn(gnChild, self.hn2(sucState) )
+
+                child.action = actionIndex
+                # INSERE NÓ FILHO NA FRONTEIRA (SE SATISFAZ CONDIÇÕES)
+
+                # Testa se estado do nó filho foi explorado
+                alreadyExplored = False
+                for node in explored:
+                    if(child.state == node):
+                        alreadyExplored = True
+                        break
+
+                # Testa se estado do nó filho está na fronteira, caso esteja
+                # guarda o nó existente em nFront
+                nodeFront = None
+                if not alreadyExplored:
+                    for node in frontier:
+                        if(child.state == node.state):
+                            nodeFront = node
+                            break
+                # Se ainda não foi explorado
+                if not alreadyExplored:
+                    # e não está na fronteira, adiciona à fronteira
+                    if nodeFront == None:
+                        frontier.append(child)
+                        frontier.sort(key=lambda x: x.getFn()) # Ordena a fronteira pelo f(n), ascendente
+                        treeNodesCt += 1
+                    else:
+                        # Se já está na fronteira temos que ver se é melhor
+                        if nodeFront.getFn() > child.getFn():       # Nó da fronteira tem custo maior que o filho
+                            frontier.remove(nodeFront)              # Remove nó da fronteira (pior e deve ser substituído)
+                            nodeFront.remove()                      # Retira-se da árvore 
+                            frontier.append(child)                  # Adiciona filho que é melhor
+                            frontier.sort(key=lambda x: x.getFn())  # Ordena a fronteira pelo f(n), ascendente
+                            # treeNodesCt não é incrementado por inclui o melhor e retira o pior
+                        else:
+                            # Conta como descartado porque o filho é pior que o nó da fronteira e foi descartado
+                            frontierDiscardedNodesCt += 1
+                else:
+                    exploredDicardedNodesCt += 1
+            
+            print("\n{0:<30}{1}".format("Nós na árvore: ",treeNodesCt))
+            print("{0:<30}{1}".format("Descartados já na fronteira: ",frontierDiscardedNodesCt))
+            print("{0:<30}{1}".format("Descartados já explorados: ",exploredDicardedNodesCt))
+            print("{0:<30}{1}".format("Total de nós gerados: ",treeNodesCt + frontierDiscardedNodesCt + exploredDicardedNodesCt))
+
+
+        if(solution != None):
+            print("!!! Solução encontrada !!!")
+            print("!!! Custo:        {}".format(solution.gn))
+            print("!!! Profundidade: {}\n".format(solution.depth))
+            print("\n{0:<30}{1}".format("Nós na árvore: ",treeNodesCt))
+            print("{0:<30}{1}".format("Descartados já na fronteira: ",frontierDiscardedNodesCt))
+            print("{0:<30}{1}".format("Descartados já explorados: ",exploredDicardedNodesCt))
+            print("{0:<30}{1}".format("Total de nós gerados: ",treeNodesCt + frontierDiscardedNodesCt + exploredDicardedNodesCt))
+            return buildPlan(solution)
+        else:
+            print("### Solução NÃO encontrada ###")
+            return None
---
+++ pkg/problem.py
@@ -0,0 +1,172 @@
+from maze import Maze
+from state import State
+from cardinal import *
+
+class Problem:
+    """Representação de um problema a ser resolvido por um algoritmo de busca clássica.
+    A formulação do problema - instância desta classe - reside na 'mente' do agente."""
+
+
+    def __init__(self):
+        self.initialState = State(0,0)
+        self.goalState = State(0,0)
+
+    def createMaze(self, maxRows, maxColumns):
+        """Este método instancia um labirinto - representa o que o agente crê ser o labirinto.
+        As paredes devem ser colocadas fora desta classe porque este.
+        @param maxRows: máximo de linhas do labirinto.
+        @param maxColumns: máximo de colunas do labirinto."""
+        self.mazeBelief = Maze(maxRows, maxColumns)
+        self.maxRows = maxRows
+        self.maxColumns = maxColumns
+        self.cost = [[0.0 for j in range(maxRows*maxColumns)]for i in range(8)]
+
+    def defInitialState(self, row, col):
+        """Define o estado inicial.
+        @param row: linha do estado inicial.
+        @param col: coluna do estado inicial."""
+        self.initialState.row = row
+        self.initialState.col = col
+
+    def defGoalState(self, row, col):
+        """Define o estado objetivo.
+        @param row: linha do estado objetivo.
+        @param col: coluna do estado objetivo."""
+        self.goalState.row = row
+        self.goalState.col = col
+
+    def suc(self, state, action):
+        """Função sucessora: recebe um estado e calcula o estado sucessor ao executar uma ação.
+        @param state: estado atual.
+        @param action: ação a ser realizado a partir do estado state.
+        @return estado sucessor"""
+        row = state.row
+        col = state.col
+
+        # Incrementa a linha e coluna de acordo com a respectiva ação
+        # rowIncrement e colIncrement estão definidos em cardinal.py
+        row += rowIncrement[action] 
+        col += colIncrement[action]
+
+        # Verifica limites
+        if row < 0:
+            row = 0
+        if col < 0:
+            col = 0
+        if row == self.mazeBelief.maxRows:
+            row = self.mazeBelief.maxRows - 1
+        if col == self.mazeBelief.maxColumns:
+            col = self.mazeBelief.maxColumns - 1
+        
+        # Se tiver parede agente fica na posição original
+        if self.mazeBelief.walls[row][col] == 1: 
+            row = state.row
+            col = state.col
+        
+        return State(row, col)
+
+    def possibleActions(self, state):
+        """Retorna as ações possíveis de serem executadas em um estado.
+        O valor retornado é um vetor de inteiros.
+        Se o valor da posição é -1 então a ação correspondente não pode ser executada, caso contrário valerá 1.
+        Exemplo: se retornar [-1, -1, -1, 1, 1, -1, -1, -1] apenas as ações 3 e 4 podem ser executadas, ou seja, apenas SE e S.
+        @param state: estado atual.
+        @return ações possíveis"""
+        
+        actions = [1,1,1,1,1,1,1,1] # Supõe que todas as ações são possíveis
+        
+        # @TODO: Implementação do aluno
+        
+        row = state.row
+        col = state.col 
+
+        # Esta no limite superior, não pode ir para N, NE ou NO
+        if row == 0: 
+            actions[N] = actions[NE] = actions[NO] = -1
+        # Esta no limite direito, não pode ir para NE, L ou SE
+        if col == self.mazeBelief.maxColumns - 1:
+            actions[NE] = actions[L] = actions[SE] = -1
+        # Esta no limite inferior, não pode ir para SE, S ou SO
+        if row == self.mazeBelief.maxRows - 1:
+            actions[SE] = actions[S] = actions[SO] = -1
+        # Esta no limite esquerdo, não pode ir para SO, O ou NO
+        if col == 0:
+            actions[SO] = actions[O] = actions[NO] = -1
+
+        walls = self.mazeBelief.walls
+        # Testa se há parede nas direções
+        if actions[N] != -1 and walls[row - 1][col] == 1: # Norte
+            actions[N] = -1
+        if actions[L] != -1 and walls[row][col + 1] == 1: # Leste
+            actions[L] = -1
+        if actions[S] != -1 and walls[row + 1][col] == 1: # Sul
+            actions[S] = -1
+        if actions[O] != -1 and walls[row ][col - 1] == 1: # Oeste
+            actions[O] = -1
+
+        if actions[NE] != -1 and walls[row - 1][col + 1] == 1: # Nordeste
+            actions[NE] = -1
+        if actions[NO] != -1 and walls[row - 1][col - 1] == 1: # Noroeste
+            actions[NO] = -1
+        if actions[SE] != -1 and walls[row + 1][col + 1] == 1: # Sudeste
+            actions[SE] = -1
+        if actions[SO] != -1 and walls[row + 1][col - 1] == 1: # Sudoeste
+            actions[SO] = -1
+
+        return actions
+
+    def possibleActionsWithoutCollaterals(self, state):
+        """Retorna as ações possíveis de serem executadas em um estado, desconsiderando movimentos na diagonal.
+        O valor retornado é um vetor de inteiros.
+        Se o valor da posição é -1 então a ação correspondente não pode ser executada, caso contrário valerá 1.
+        Exemplo: se retornar [1, -1, -1, -1, -1, -1, -1, -1] apenas a ação 0 pode ser executada, ou seja, apena N.
+        @param state: estado atual.
+        @return ações possíveis"""
+        
+        actions = [1,-1,1,-1,1,-1,1,-1] # Supõe que todas as ações (menos na diagonal) são possíveis
+
+        # @TODO: Implementação do aluno
+
+        row = state.row
+        col = state.col 
+
+        # Esta no limite superior, não pode ir para N
+        if row == 0: 
+            actions[N] = -1
+        # Esta no limite direito, não pode ir para L
+        if col == self.mazeBelief.maxColumns - 1:
+            actions[L] = -1
+        # Esta no limite inferior, não pode ir para S
+        if row == self.mazeBelief.maxRows - 1:
+            actions[S]= -1
+        # Esta no limite esquerdo, não pode ir para O
+        if col == 0:
+            actions[O] = -1
+
+        walls = self.mazeBelief.walls
+        # Testa se há parede nas direções
+        if walls[row - 1][col] == 1: # Norte
+            actions[N] = -1
+        if walls[row][col + 1] == 1: # Leste
+            actions[L] = -1
+        if walls[row + 1][col] == 1: # Sul
+            actions[S] = -1
+        if walls[row ][col - 1] == 1: # Oeste
+            actions[O] = -1
+
+        return actions
+
+    def getActionCost(self, action):
+        """Retorna o custo da ação.
+        @param action:
+        @return custo da ação"""
+        if (action == N or action == L or action == O or action == S):
+            return 1.0
+        else:
+            return 1.5
+
+    def goalTest(self, currentState):
+        """Testa se alcançou o estado objetivo.
+        @param currentState: estado atual.
+        @return True se o estado atual for igual ao estado objetivo."""
+        return currentState == self.goalState # Operador de igualdade definido em __eq__ no arquivo state.py
